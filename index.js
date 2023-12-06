12884d6f39f9193563ef4337c0c3bd64513b5a67
Komalpreet Singh <komalpreetsingh2305@gmail.com>
2023-12-06 03:21:50 -0500

    functionality implemented

--- index.js
+++ index.js
@@ -6,5 +6,28 @@ const btn = document.getElementById("search-btn");
 btn.addEventListener("click", () => {
     let inpWord = document.getElementById("inp-word").value;
     console.log(inpWord);
-    fetch(`${url}${inpWord}`).then((res) => res.json).then((data) => console.log(data));
+    fetch(`${url}${inpWord}`).then((res) => res.json()).then((data) => {
+        console.log(data);
+        result.innerHTML = `<div class="word">
+        <h3>${inpWord}</h3>
+        <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
+      </div>
+      <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>${data[0].phonetics[1].text}</p>
+      </div>
+      <p class="word-meaning">
+      
+      </p>
+      <p class="word-example">
+      ${data[0].meanings[0].definitions[0].definition}
+      </p>`
+        sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+    }).catch(() => {
+        result.innerHTML = `<h3 class="error">Couldn't find the word</h3>`
+    })
 })
+
+function playSound() {
+    sound.play();
+}
